25ed988caf0a8f20d8f18e3c0ea3d9df5e95b4e6
lain <87864967+Spikebybike@users.noreply.github.com>
2025-10-10 20:34:43 +0800

    Update TD.m

--- utils/TD.m
+++ utils/TD.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% Function ID for Truncated-DCT
+%%% Function TD for Truncated-DCT
 %%% Developed by Zhuohang Tan, Sichuan University
 %%% 2025-10-10
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -7,3 +7,4 @@ function TDS=TD(S,L)
 TDS=dct(S,[],2);
 TDS=TDS(:,1:L);
 end
+
