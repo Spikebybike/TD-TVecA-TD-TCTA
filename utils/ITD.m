f9521b8163c2592280107c1368f29f8cb4ccecd3
lain <87864967+Spikebybike@users.noreply.github.com>
2025-10-10 20:34:30 +0800

    Update ITD.m

--- utils/ITD.m
+++ utils/ITD.m
@@ -1,3 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Function ITD for zero-padding and IDCT
+%%% Developed by Zhuohang Tan, Sichuan University
+%%% 2025-10-10
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function ITDS=ITD(S,N,L)
 B=zeros(size(S,1),N-L);
 ITDS=cat(2,S,B);
