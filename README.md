73c424567671459e4f334ea98beed53172989f77
lain <87864967+Spikebybike@users.noreply.github.com>
2025-10-10 14:30:10 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,25 @@
+# TD-TVecA / TD-TCTA
+
+Code for ___Extreme Structured Sparse Millimeter-Wave 3-D SAR Imaging via Truncated-DCT and Toeplitz matrix methods___
+
+## Workflow of TD-TVecA / TD-TCTA for sparse 3-D SAR imaging
+<img src="https://github.com/Spikebybike/TD-TVecA-TCTA/blob/main/pic/proposed%20workflow.png" width="700px">
+
+All experiments are conducted using MATLAB r2018a.
+
+## Example of TD-TVecA / TD-TCTA
+
+<img src="https://github.com/Spikebybike/TD-TVecA-TCTA/blob/main/pic/proposed%20example.png" width="700px">
+
+## 3-D SAR imaging
+
+<img src="https://github.com/Spikebybike/TD-TVecA-TCTA/blob/main/pic/3d%20sar.png" width="700px">
+ 
+## Multiple targets
+
+<img src="https://github.com/Spikebybike/TD-TVecA-TCTA/blob/main/pic/targets.png" width="700px">
+
+## Echoes and imaging results ( 30% non-uniform row undersample ) 
+<img src="https://github.com/Spikebybike/TD-TVecA-TCTA/blob/main/pic/non-uniform_row_undersample.png" width="700px">
+
+## The code in coming soon
---
+++ main_TD_TVecA_TCTA.m
@@ -0,0 +1,86 @@
+clear
+close all
+clc
+tic;
+
+%% load data
+addpath('utils');
+data=load('data\sardata.mat');data=data.sardata;
+sarData=data.range_slice;figure;imagesc(abs(sarData));    %range slice echo
+xyz=data.slice_xyz;                                       %dx dy and distance z0
+params=data.params;
+
+%% imaging parameters
+dx=xyz.dx;
+dy=xyz.dy;                        % Sampling interval at x,y axis in mm
+z0=xyz.distance;                  %distance of radar and target at mm
+f0=params.f0;                     % start frequency
+c=params.c;                       % speed of light
+fslope=params.fslope;             % Slope const (Hz/sec)
+fs=params.fs;                     % Sampling rate (sps)
+num_sample=params.num_sample;     % Number of sampling points at z axis
+nFFTspace=params.nFFTspace;       % Number of FFT points for wave-domain
+
+k = 2*pi*f0/c;                    %wave number
+imSize =400;                      %image size at mm
+amplitude=-40;                    % amplitude of [-40,0] dB after normalization
+
+%% MASK of different sparse sampling
+[M,N]= size(sarData);mask=zeros(M,N);truncated=floor(N/2);
+
+% uniform row undersample
+masknum=1;mask([1:masknum:M],:)=1;     
+
+% 30% non-uniform row undersample
+% mask([1,3,10,12,16,20,25,26,30,32,36,39,44,48,50,52,57,61,66,68,70,72,75,79,82,84,87,88,93,97],:)=1;
+
+sarData=sarData.*mask;figure;imagesc(abs(sarData));     % mask
+fprintf('percent=%.6f\n',nnz(sarData)/numel(sarData));  % percent
+maxMod=max(abs(sarData(:)));sarData=sarData/maxMod;     %normalization
+
+%% Truncated-DCT  (TD)
+sarData=dct(sarData,[],2);  %DCT
+figure;imagesc(abs(sarData));
+
+sarData=sarData(:,1:truncated);  %truncation
+figure;imagesc(abs(sarData));
+
+%% Matrix Completion  (TVecA/TCTA)
+
+% proposed TVecA        
+% R=80;   % pencil parameter
+% mu=2;           %penalty parameter
+% e_rank=20;     %e_rank
+% K=30;           %iterations
+% sarData=TVecA(sarData,R,mu,K,e_rank);
+
+% proposed TCTA
+%P=10;  % pencil parameter
+%Q=P;            % pencil parameter
+%mu=2;           %penalty parameter
+%e_rank=20;     %e_rank 
+%K=30;           %iterations
+%sarData=TCTA(sarData,P,Q,mu,K,e_rank);
+
+figure;imagesc(abs(sarData));
+
+%% zero-padding and IDCT
+sar=zeros(M,N);
+sar(:,1:truncated)=sarData;      %zero-padding
+sarData=sar;
+figure;imagesc(abs(sarData));
+
+sarData=idct(sarData,[],2);         %idct
+figure;imagesc(abs(sarData));
+
+%% rma 2d imaging
+rma_2d(dx,dy,k,z0,sarData,nFFTspace,amplitude,imSize); %imaging of one range slice 
+
+elapsedTime = toc ;
+
+
+
+
+
+
+
