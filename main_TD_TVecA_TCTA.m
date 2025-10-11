38ceb3611b85b1cae01ee5c190171f1f5a7a556e
lain <87864967+Spikebybike@users.noreply.github.com>
2025-10-11 13:57:29 +0800

    Update main_TD_TVecA_TCTA.m

--- main_TD_TVecA_TCTA.m
+++ main_TD_TVecA_TCTA.m
@@ -36,7 +36,7 @@ amplitude=-40;                    % amplitude of [-40,0] dB after normalization
 % 30% non-uniform row undersample
 mask([1,3,10,12,16,20,25,26,30,32,36,39,44,48,50,52,57,61,66,68,70,72,75,79,82,84,87,88,93,97],:)=1;
 
-sarData=sarData.*mask;figure;imagesc(abs(sarData));     % mask
+%sarData=sarData.*mask;figure;imagesc(abs(sarData));     % mask
 fprintf('percent=%.6f\n',nnz(sarData)/numel(sarData));  % percent
 maxMod=max(abs(sarData(:)));sarData=sarData/maxMod;     %normalization
 
@@ -48,11 +48,11 @@ figure;imagesc(abs(sarData));
 %% Matrix Completion  (TVecA/TCTA)
 
 % proposed TVecA        
-R=80;   % pencil parameter
-mu=2;           %penalty parameter
-e_rank=R/4;     %e_rank
-K=30;           %iterations
-sarData=TVecA(sarData,R,mu,K,e_rank);
+%R=80;   % pencil parameter
+%mu=2;           %penalty parameter
+%e_rank=R/4;     %e_rank
+%K=30;           %iterations
+%sarData=TVecA(sarData,R,mu,K,e_rank);
 
 % proposed TCTA
 % P=10;  % pencil parameter
@@ -74,4 +74,5 @@ figure;imagesc(abs(sarData));
 rma_2d(dx,dy,k,z0,sarData,nFFTspace,amplitude,imSize); %imaging of one range slice 
 
 elapsedTime = toc ;
+
 
